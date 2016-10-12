1ad910b413d4f96997a67ea95599749396adf4a2
orion2166 <orion030591>
2016-10-12 14:01:37 -0500

    https trial

--- scripts/propigation.js
+++ scripts/propigation.js
@@ -1,13 +1,13 @@
-var SEMANTIC_SERVICE_URL = "http://ecology-service.cse.tamu.edu/BigSemanticsService/";
+var SEMANTIC_SERVICE_URL = "https://ecology-service.cse.tamu.edu/BigSemanticsService/";
 global_storage = "";
 global_products = 
-    ["http://www.amazon.com/HP-14-an013nr-14-Inch-Notebook-Drive/dp/B01F4ZG68A/ref=sr_1_1?s=pc&ie=UTF8&qid=1476280219&sr=1-1&keywords=laptop",
-     "http://www.amazon.com/HP-14-an013nr-14-Inch-Notebook-Drive/dp/B01F4ZG68A/ref=sr_1_3?s=pc&ie=UTF8&qid=1476283632&sr=1-3&keywords=Laptop",
-     "http://www.amazon.com/Acer-Chromebook-CB3-131-C3SZ-11-6-Inch-Dual-Core/dp/B019G7VPTC/ref=sr_1_5?s=pc&ie=UTF8&qid=1476283632&sr=1-5&keywords=Laptop",
-     "http://www.walmart.com/ip/HP-Flyer-Red-15.6-15-f272wm-Laptop-PC-with-Intel-Pentium-N3540-Processor-4GB-Memory-500GB-Hard-Drive-and-Windows-10-Home/46429958",
-     "http://www.walmart.com/ip/Refurbished-Apple-MacBook-Air-11-6-LED-Intel-i5-3317-1-7GHz-4GB-64GB-SSD-Notebook-MD223LL/165231312",
-     "http//www.newegg.com/Product/Product.aspx?Item=1TS-001A-002P7",
-     "http://www.newegg.com/Product/Product.aspx?Item=N82E16834319906"]
+    ["https://www.amazon.com/HP-14-an013nr-14-Inch-Notebook-Drive/dp/B01F4ZG68A/ref=sr_1_1?s=pc&ie=UTF8&qid=1476280219&sr=1-1&keywords=laptop",
+     "https://www.amazon.com/HP-14-an013nr-14-Inch-Notebook-Drive/dp/B01F4ZG68A/ref=sr_1_3?s=pc&ie=UTF8&qid=1476283632&sr=1-3&keywords=Laptop",
+     "https://www.amazon.com/Acer-Chromebook-CB3-131-C3SZ-11-6-Inch-Dual-Core/dp/B019G7VPTC/ref=sr_1_5?s=pc&ie=UTF8&qid=1476283632&sr=1-5&keywords=Laptop",
+     "https://www.walmart.com/ip/HP-Flyer-Red-15.6-15-f272wm-Laptop-PC-with-Intel-Pentium-N3540-Processor-4GB-Memory-500GB-Hard-Drive-and-Windows-10-Home/46429958",
+     "https://www.walmart.com/ip/Refurbished-Apple-MacBook-Air-11-6-LED-Intel-i5-3317-1-7GHz-4GB-64GB-SSD-Notebook-MD223LL/165231312",
+     "https://www.newegg.com/Product/Product.aspx?Item=1TS-001A-002P7",
+     "https://www.newegg.com/Product/Product.aspx?Item=N82E16834319906"]
 
 global_metadata_from_array = [];
 var metadata_index = 0;
