fbe5d2910329e226d5454100b078545b5201c359
roguelider <roguelider@live.com>
2016-10-30 22:21:22 -0600

    dropdown cart
    adding feature of dropping down cart (ignore netbeans files... is just
    an IDE stuff)

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,19 +1,37 @@
 // JavaScript Document
 // Get the modal
 var modal;
+var cart;
+var button;
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 function goToCheckout(){
-	modal = document.getElementById('checkout_popup');
+    modal = document.getElementById('checkout_popup');
     modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 function closeCheckout() {
     modal.style.display = "none";
+}
+
+// When the user clicks on the button, open the modal 
+function displayButton(){
+    cart = document.getElementById('cart-table');
+    cart.style.display = "block";
+    button = document.getElementById('buy_button');
+    button.style.display = "block";
+    document.getElementById('cart_header').onclick = hideButton;
+}
+
+// When the user clicks on <span> (x), close the modal
+function hideButton() {
+    cart.style.display = "none";
+    button.style.display = "none";
+    document.getElementById('cart_header').onclick = displayButton;
 }
 
 function checkout(){
